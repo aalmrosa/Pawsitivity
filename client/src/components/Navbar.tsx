880546e0ffc7f2f86ca52e09e5aee87f2f47fb1d
nyang5504 <yang.nina5504@gmail.com>
2024-10-22 23:19:41 -0400

    mobile nav dropdown

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -16,16 +16,41 @@ const Navbar: React.FC<Props> = () => {
     return (
         <div className={styles['nav-container']}>
             <div id={styles['logo-container']}><img id={styles['logo']} src={logo} alt='pawsitivity logo'/></div>
-            <input placeholder={'Search'} id={styles['search-bar']}/>
-            <div className={styles['nav-items']}>
-                <i className={`material-icons-outlined ${styles['nav-item']}`}>home</i>
-                <i className={`material-icons-outlined ${styles['nav-item']}`}>person</i>
-                <i className={`material-icons-outlined ${styles['nav-item']}`}>notifications</i>
-                <i className={`material-icons-outlined ${styles['nav-item']}`}>settings</i>
-                <i className={`material-icons-outlined ${styles['nav-item']} ${styles['closed']}`} onClick={toggleMenu}>
-                    {icon}
-                </i>
+
+            <div id={styles['search-container']}>
+                <input placeholder={'Search'} id={styles['search-bar']}/>
+                <i id={styles['search-icon']} className={'material-icons-outlined'}>search</i>
             </div>
+
+
+            <ul className={`${styles['nav-items']} ${icon === "menu_open" ? styles.active : ""}`}>
+                <li className={styles['nav-item']}>
+                    <i className={'material-icons-outlined'}>home</i>
+                    <span>Home</span>
+                </li>
+                <li className={styles['nav-item']}>
+                    <i className={'material-icons-outlined'}>person</i>
+                    <span>Profile</span>
+                </li>
+                <li className={styles['nav-item']}>
+                    <i className={'material-icons-outlined'}>notifications</i>
+                    <span>Notifications</span>
+                </li>
+                <li className={styles['nav-item']}>
+                    <i className={'material-icons-outlined'}>settings</i>
+                    <span>Settings</span>
+                </li>
+
+                {/*<i className={`material-icons-outlined ${styles['nav-item']}`}>home</i>*/}
+                {/*<i className={`material-icons-outlined ${styles['nav-item']}`}>person</i>*/}
+                {/*<i className={`material-icons-outlined ${styles['nav-item']}`}>notifications</i>*/}
+                {/*<i className={`material-icons-outlined ${styles['nav-item']}`}>settings</i>*/}
+            </ul>
+
+            <i className={`material-icons-outlined ${styles['closed']} ${styles['hamburger']}`}
+               onClick={toggleMenu}>
+                {icon}
+            </i>
         </div>
 
     );
