207a96a498c7ae2ebdb455beb58e41cc2c51c193
Augusto Rosa <augustoalmrosa@gmail.com>
2024-10-30 18:45:17 -0400

    incomplete homepage refactor, merge to login/registration once done

--- client/src/components/homepage/Homepage.tsx
+++ client/src/components/homepage/Homepage.tsx
@@ -9,45 +9,32 @@ interface Props {
 const Homepage: React.FC<Props> = () => {
 
     return (
-        <div id={styles["homepage-container"]}>
-            {/* <div id={styles["right-contents"]}>
-                <div id={styles["trending"]}>
-                    <h2>Trending Mews</h2>
-                </div>
-                <div id={styles["create-post"]}>
+        <div className={styles["container"]}>
+            <div className={styles["sticky-panels"]}>
+                <section className={styles["side-profile-container"]}>
+                <SideProfile/>
+                </section>
+
+                <section className={styles['right-container']}>
+                    <div style={{backgroundColor: "white", height: "600px", borderRadius: "15px"}}>Hello</div>
+                </section>
+            </div>
+            <section className={styles["feed-container"]}>
+                <section className={styles["create-post"]}>
                     <div className={styles["prof-pic-container"]}>
                         <img className={styles["prof-pic"]}
-                             src="https://images.unsplash.com/photo-1561948955-570b270e7c36?q=80&w=1802&a
-                         uto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                             alt="profile picture"/>
-                    </div>
-                    <textarea id={styles["post-text"]}
-                              placeholder={"What are your pet peeves regarding humans? Do share!"}></textarea>
-                </div>
-            </div> */}
-            <section id={styles["side-profile-container"]}>
-               <SideProfile/>
-            </section>
-            <section>
-                <div id={styles["create-post"]}>
-                    <div className={styles["prof-pic-container"]}>
-                        <img className={styles["prof-pic"]}
-                            src="https://images.unsplash.com/photo-1561948955-570b270e7c36?q=80&w=1802&a
-                            uto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            src="https://images.unsplash.com/photo-1561948955-570b270e7c36?q=80&w=1802&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="profile picture"/>
                     </div>
-                    <textarea id={styles["post-text"]}
-                              placeholder={"What are your pet peeves regarding humans? Do share!"}></textarea>
-                </div>
+                    <textarea className={styles["new-post-text"]}
+                        placeholder={"What are your pet peeves regarding humans? Do share!"}></textarea>
+                </section>
+                <section className={styles["feed-posts-container"]}>
+                    <Post/>
+                    <Post/>
+                </section>
+                <span className={styles["load-btn"]}>Load more</span>
             </section>
-            <section id={styles['right-container']}>
-                <div style={{backgroundColor: "white", height: "600px", borderRadius: "15px"}}>Hello</div>
-            </section>
-            <div id={styles["feed-container"]}>
-                <Post/>
-                <Post/>
-            </div>
-            
         </div>
     );
 };
