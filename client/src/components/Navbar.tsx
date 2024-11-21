5ea815b72da41d2eb748d5d37565bf2bc52a3706
Augusto Rosa <augustoalmrosa@gmail.com>
2024-11-21 15:40:53 -0500

    [frontend] feat(navbar): added dropdown menu for settings on desktop and routing for endpoints

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,22 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import logo from "../pawsitivity-icon.png";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
+
 interface Props {
 
 }
 
 const Navbar: React.FC<Props> = () => {
 
-    const [icon, setIcon] = useState<string>("menu")
-
+    const [icon, setIcon] = useState<string>("menu");
     const toggleMenu = () => {
         icon === "menu" ? setIcon("menu_open") : setIcon("menu");
+    }
+
+    const [state, setState] = useState<string>("closed");
+    const toggleSettings = () => {
+        state === "closed" ? setState("open") : setState("closed");
     }
 
     return (
@@ -44,20 +49,33 @@ const Navbar: React.FC<Props> = () => {
                         <span>Notifications</span>
                     </Link>
                 </li>
-                <li className={styles['nav-item']}>
-                    <Link to="/">
+                <li className={`${styles['nav-item']} ${styles["mobile-only"]}`} >
+                    <Link to="/settings">
                         <i className={'material-symbols-outlined'}>settings</i>
                         <span>Settings</span>
                     </Link>
                 </li>
+                <li className={`${styles['nav-item']} ${styles["desktop-only"]}`} onClick={toggleSettings}>
+                    <a>
+                        <i className={'material-symbols-outlined'}>settings</i>
+                        <span>Settings</span>
+                    </a>
+                </li>
+                <div className={`${styles["settings-menu"]} ${state === "open" ? styles.active : "closed"}`}>
+                    <ul>
+                        <li className={styles["stg-link"]}><Link to="/settings"><span>Settings</span></Link></li>
+                        <li className={styles["stg-link"]}><Link to="/login"><span>Login</span></Link></li>
+                        <li className={styles["stg-link"]}><Link to="/signup"><span>Sign-up</span></Link></li>
+                        <li className={styles["stg-link"]}><Link to="/logout"><span>Logout</span></Link></li>
+                    </ul>
+                </div>
             </ul>
-
-            <i className={`material-symbols-outlined ${styles['closed']} ${styles['hamburger']}`}
+            
+            <i className={`material-symbols-outlined ${styles['hamburger']}`}
                onClick={toggleMenu}>
                 {icon}
             </i>
         </div>
-
     );
 };
 
